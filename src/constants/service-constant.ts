d0e32577e38eef0cfcf61cd010d72e0d303d01d3
rizkyaziz <rizkyaziz.hsn@gmail.com>
2025-08-12 14:29:39 +0700

    ref: set animation for image services

---
+++ src/constants/service-constant.ts
@@ -0,0 +1,58 @@
+// service-constant.ts
+export type ServiceItem = {
+  icon: string;
+  title: string;
+  content: string;
+};
+
+export const SERVICE_LIST: ServiceItem[] = [
+  {
+    icon: "/assets/icons/ic-kitchens.svg",
+    title: "Kitchens",
+    content:
+      "At LifetimeArt, we design and build stunning kitchens tailored to your style and needs. Whether you're after a sleek modern space or a classic, timeless look, our expert team delivers high-quality craftsmanship, functionality, and attention to detail to create the heart of your home.",
+  },
+  {
+    icon: "/assets/icons/ic-loft-conversions.svg",
+    title: "Loft Conversions",
+    content:
+      "At LifetimeArt, we design and build stunning kitchens tailored to your style and needs. Whether you're after a sleek modern space or a classic, timeless look, our expert team delivers high-quality craftsmanship, functionality, and attention to detail to create the heart of your home.",
+  },
+  {
+    icon: "/assets/icons/ic-bathrooms.svg",
+    title: "Bathrooms",
+    content:
+      "At LifetimeArt, we design and build stunning kitchens tailored to your style and needs. Whether you're after a sleek modern space or a classic, timeless look, our expert team delivers high-quality craftsmanship, functionality, and attention to detail to create the heart of your home.",
+  },
+  {
+    icon: "/assets/icons/ic-extensions.svg",
+    title: "Extensions",
+    content:
+      "At LifetimeArt, we design and build stunning kitchens tailored to your style and needs. Whether you're after a sleek modern space or a classic, timeless look, our expert team delivers high-quality craftsmanship, functionality, and attention to detail to create the heart of your home.",
+  },
+  {
+    icon: "/assets/icons/ic-restorations.svg",
+    title: "Restorations",
+    content:
+      "At LifetimeArt, we design and build stunning kitchens tailored to your style and needs. Whether you're after a sleek modern space or a classic, timeless look, our expert team delivers high-quality craftsmanship, functionality, and attention to detail to create the heart of your home.",
+  },
+  {
+    icon: "/assets/icons/ic-external-works.svg",
+    title: "External Works",
+    content:
+      "At LifetimeArt, we design and build stunning kitchens tailored to your style and needs. Whether you're after a sleek modern space or a classic, timeless look, our expert team delivers high-quality craftsmanship, functionality, and attention to detail to create the heart of your home.",
+  },
+];
+
+
+export const SERVICE_CONTENT = {
+  badgeTitle: "Services",
+  title: "What we do",
+  description: "Find out which one of our services fit the needs of your project",
+  image: {
+    src: "/assets/img/services/image-1.jpg",
+    width: 700,
+    height: 685,
+    alt: "Services image",
+  },
+};
--- src/modules/services/_components/Service.tsx
+++ src/modules/services/_components/Service.tsx
@@ -5,71 +5,34 @@ import Badge from "@/components/common/Badge";
 import Image from "next/image";
 import { useState } from "react";
 import { useServiceAnimation } from "./service.animation";
+import { SERVICE_CONTENT, SERVICE_LIST } from "@/constants/service-constant";
 
 const Service = () => {
   const { serviceRef } = useServiceAnimation();
   const [openIndex, setOpenIndex] = useState<number | null>(0);
 
-  const accordionData = [
-    {
-      icon: "/assets/icons/ic-kitchens.svg",
-      title: "Kitchens",
-      content:
-        "At LifetimeArt, we design and build stunning kitchens tailored to your style and needs. Whether you're after a sleek modern space or a classic, timeless look, our expert team delivers high-quality craftsmanship, functionality, and attention to detail to create the heart of your home.",
-    },
-    {
-      icon: "/assets/icons/ic-loft-conversions.svg",
-      title: "Loft Conversions",
-      content:
-        "At LifetimeArt, we design and build stunning kitchens tailored to your style and needs. Whether you're after a sleek modern space or a classic, timeless look, our expert team delivers high-quality craftsmanship, functionality, and attention to detail to create the heart of your home.",
-    },
-    {
-      icon: "/assets/icons/ic-bathrooms.svg",
-      title: "Bathrooms",
-      content:
-        "At LifetimeArt, we design and build stunning kitchens tailored to your style and needs. Whether you're after a sleek modern space or a classic, timeless look, our expert team delivers high-quality craftsmanship, functionality, and attention to detail to create the heart of your home.",
-    },
-    {
-      icon: "/assets/icons/ic-extensions.svg",
-      title: "Extensions",
-      content:
-        "At LifetimeArt, we design and build stunning kitchens tailored to your style and needs. Whether you're after a sleek modern space or a classic, timeless look, our expert team delivers high-quality craftsmanship, functionality, and attention to detail to create the heart of your home.",
-    },
-    {
-      icon: "/assets/icons/ic-restorations.svg",
-      title: "Restorations",
-      content:
-        "At LifetimeArt, we design and build stunning kitchens tailored to your style and needs. Whether you're after a sleek modern space or a classic, timeless look, our expert team delivers high-quality craftsmanship, functionality, and attention to detail to create the heart of your home.",
-    },
-    {
-      icon: "/assets/icons/ic-external-works.svg",
-      title: "External Works",
-      content:
-        "At LifetimeArt, we design and build stunning kitchens tailored to your style and needs. Whether you're after a sleek modern space or a classic, timeless look, our expert team delivers high-quality craftsmanship, functionality, and attention to detail to create the heart of your home.",
-    },
-  ];
   return (
     <div ref={serviceRef} className="bg-[#FAFAFA] py-[120px]">
       <div className="container">
         <div className="flex flex-col justify-center items-center">
           <div className="service-badge">
-            <Badge title="Services" />
+            <Badge title={SERVICE_CONTENT.badgeTitle} />
           </div>
           <h2 className="service-title text-[#101014] font-medium text-5xl mt-2">
-            What we do
+            {SERVICE_CONTENT.title}
           </h2>
-          <p className="service-paragraph text-xl text-[#3D3D47] mt-4">
-            Find out which one of our services fit the needs of your project
+          <p className="service-description text-xl text-[#3D3D47] mt-4">
+            {SERVICE_CONTENT.description}
           </p>
         </div>
 
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-20 mt-24 items-center">
-          <div className="overflow-hidden rounded-xl">
+          <div className="service-image overflow-hidden rounded-xl">
             <Image
-              src={"/assets/img/services/image-1.jpg"}
-              width={700}
-              height={685}
-              alt="Services image"
+              src={SERVICE_CONTENT.image.src}
+              width={SERVICE_CONTENT.image.width}
+              height={SERVICE_CONTENT.image.height}
+              alt={SERVICE_CONTENT.image.alt}
               className={[
                 "rounded-xl object-cover transition duration-500",
                 openIndex !== null ? "scale-105" : "scale-100",
@@ -78,7 +41,7 @@ const Service = () => {
           </div>
           {/* Accordion items */}
           <div>
-            {accordionData.map((item, index) => (
+            {SERVICE_LIST.map((item, index) => (
               <Accordion
                 key={index}
                 icon={item.icon}
